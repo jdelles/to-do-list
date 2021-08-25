746b46b3ac5820ccc3479371eaa3158b1869a8a6
jdelles <james.delles@gmail.com>
2021-08-25 07:52:03 -0400

    created basic todo objects and projects

--- src/project/project.js
+++ src/project/project.js
@@ -1,14 +1,28 @@
+import createTodo from './todos/todos.js'; 
+
 const project = (name) => {
     let todos = new Map(); 
 
-    const addTodo = (name, todo) => {
-        todos.set(name, todo); 
+    const addTodo = (title, description, date, priority) => {
+        if (validateTitle()) {
+            const todo = createTodo(title, description, date, priority); 
+            todos.set(title, todo); 
+        } else {
+            alert("ToDos must have a unique name"); 
+        }
     }
 
-    const removeTodo = (name) => {
+    const removeTodo = (title) => {
         todos.delete(name); 
+    }
+
+    const validateTitle = (title) => {
+        if (todos.get(title) === undefined) {
+            return true; 
+        } else {
+            return false; 
+        }; 
     }
 
     return {name, addTodo, removeTodo}; 
 }
-
